9f29c74d62cd522b27845026f73d0a96d0563027
adamacneill <adamichelee@gmail.com>
2023-03-20 17:30:42 -0700

    initial commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          BankAccount ba = new BankAccount();
+
+
+            Console.WriteLine("How much $ would you like to deposit today?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+            double userBalance = ba.getBalance();
+
+            Console.WriteLine($"Your current balance is: {userBalance,0:c}");
         }
     }
 }
